81d1ce30ca2b757534c7215d6079c49ebc4a09ef
wowthedoge <honggggg2023@gmail.com>
2025-05-27 19:44:58 +0800

    modified project description

--- pages/projectsPage.js
+++ pages/projectsPage.js
@@ -3,9 +3,9 @@ const projects = [
     title: "JIT compiler in Haskell and C",
     description: 
 `
-I've been curious about runtimes for a while now - how the JVM/CLR works, multithreading, event loops, async and coroutines, garbage collection. This project is only to learn about those - I'm not releasing a new language anytime soon (though that would be cool). 
+I've been curious about runtimes for a while now - how the JVM/similar virtual machine runtimes work, multithreading, event loops, async and coroutines, garbage collection. This project is to learn about those.
 
-This is my second attempt - in my first attempt I bit off a bit too much and attempted it in C++ with LLVM, though I didn't have much experience with low-level programming. This time I started with Haskell (I love Haskell’s syntax and a functional language is a little nicer for a recursive parsing) compiling into bytecode for a C-based interpreter that manually manages memory. That's all, though - I ain't touching assembly (yet?)`,
+This is my second attempt - in my first attempt I bit off a bit too much and attempted it in C++ with LLVM, though I didn't have much experience with low-level programming. This time I started with Haskell (I love Haskell’s syntax and a functional language is a little nicer for a recursive parsing) compiling into bytecode for a C-based interpreter that manually manages memory.`,
     tags: ["Haskell", "C", "Compilers"],
     links: [
       {
